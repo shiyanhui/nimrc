b8dc4aff59244442d9fa4bb58bafe111f08eec6e
shiyanhui <lime.syh@gmail.com>
2025-04-13 15:48:03 +0800

    feat: add monokai-pro, dropbar and codecompanion and so on

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -4,5 +4,7 @@ require("lualine").setup({
   options = {
     theme = common.colorscheme.get_name(),
     disabled_filetypes = {"packer", "NvimTree"},
-  }
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" }
+  },
 })
